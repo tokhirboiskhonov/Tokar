01bf02c101e5e0ad03fa15f3f5615324b3404ae0
Shukhratovich-a <shukhratovich138@gmail.com>
2021-12-21 14:17:53 +0500

    Edit header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const elHamburger =  document.querySelector('.burger')
-const elHeader  = document.querySelector('.body')
- 
+const elHamburger = document.querySelector('.burger')
+const elHeader = document.querySelector('.body')
+
 elHamburger.addEventListener('click', () => {
     elHeader.classList.toggle('body--active')
 })
